20ece1d82200a5c34300acd4c67ed7f447b12e59
Thorsten Sick <thorsten.sick@avira.com>
2014-10-06 09:27:38 +0200

    Adding process count

--- utils/logcompare.py
+++ utils/logcompare.py
@@ -8,33 +8,38 @@ import json
 
 class Compare():
     def __init__(self):
-        self.base_raw = None
-        self.new_raw = None
-        self.base_apis = None
-        self.new_apis = None
+        self.base = {}
+        self.new = {}
 
     def add_report(self, filename, base=True):
         with open(filename)as fh:
             data = json.load(fh)
             if base:
-                self.base_raw = data
+                self.base["raw"] = data
             else:
-                self.new_raw = data
+                self.new["raw"] = data
 
     def _parse_old(self, report):
         apis = []
+        processes = []
+        pcount = 0
         for p in report["behavior"]["processes"]:
-                for c in p["calls"]:
-                    apis.append(c["api"])
-        return apis
+            for c in p["calls"]:
+                apis.append(c["api"])
+            pcount += 1
+        self.base["apis"] = apis
+        self.base["pcount"] = pcount
 
     def _parse_new(self, report):
         apis = []
+        pcount = 0
         for pid in report["behavior2"]["processes"][0]["processes"]:
+            pcount += 1
             for tid in report["behavior2"]["processes"][0]["processes"][pid]["calls"]:
                 for c in report["behavior2"]["processes"][0]["processes"][pid]["calls"][tid]:
                     apis.append(c["api"])
-        return apis
+        self.new["apis"] = apis
+        self.new["pcount"] = pcount
 
     def _parse_one(self, report):
         if "behavior" in report:
@@ -47,18 +52,20 @@ class Compare():
         """ Parse reports
         :return:
         """
-
-        self.base_apis = self._parse_one(self.base_raw)
-        self.new_apis = self._parse_one(self.new_raw)
+        self._parse_one(self.base["raw"])
+        self._parse_one(self.new["raw"])
 
     def find_first_crashes(self):
+        """ Identify API calls in old, but not in new, sorted by appearance in old. These are the first suspects for crashes
+
+        :return:
+        """
         crashes = []
-        for i in self.base_apis:
-            if not i in self.new_apis:
+        for i in self.base["apis"]:
+            if not i in self.new["apis"]:
                 if not i in crashes:
                     crashes.append(i)
         return crashes
-
 
 
 
@@ -70,6 +77,7 @@ if __name__ == "__main__":
     parser.add_argument('base', help='the base report')
     parser.add_argument('new', help='the new one')
     parser.add_argument("--missing_api", default=False, action="store_true", help="List API calls in base that are not in new. chronological ordering. Start fixing them from top to bottom.")
+    parser.add_argument("--processes", default=False, action="store_true", help="List monitored processes")
 
     args = parser.parse_args()
     c = Compare()
@@ -78,3 +86,5 @@ if __name__ == "__main__":
     c.parse()
     if args.missing_api:
         print ("\n".join(c.find_first_crashes()))
+    if args.processes:
+        print ("Number processes Old: %d New %d\n" % (c.base["pcount"], c.new["pcount"]))
